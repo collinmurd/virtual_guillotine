e0336e3494ec85a8adf0cbec00f84158b6f2465e
collinmurd <collinmurdock22@gmail.com>
2024-09-22 15:17:23 -0400

    table with current scores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-import { getLeague, getLeagueStatsForWeek } from "@/yahooApi";
+import { getSession } from "@/session";
+import { getLeagueStatsForWeek } from "@/yahooApi";
+import { cookies } from "next/headers";
 
 export default async function Scoreboard() {
-  const leagueStats = await getLeagueStatsForWeek();
+  if (!(await getSession(cookies()))) {
+    console.log('no session')
+    return <></>
+  }
 
+  const leagueStats = await getLeagueStatsForWeek();
   const tableData = leagueStats.teams.map((team: any) => {
     return (
-      <tr>
-        <th scope="row">{team.name}</th>
+      <tr key={team.team.team_id}>
+        <th scope="row">{team.team.name}</th>
+        <td>{team.team.team_points.total}</td>
         <td></td>
       </tr>
     )
--- src/yahooApi.ts
+++ src/yahooApi.ts
@@ -12,24 +12,24 @@ async function getAuth() {
 
 export async function getLeague() {
   const resp = await fetch(
-    `https://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.${constants.LEAGUE_ID}`, {
+    `https://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.${constants.LEAGUE_ID}?format=json_f`, {
       headers: {
         'Authorization': await getAuth()
       }
     }
   );
 
-  return (await parseStringPromise(await resp.text())).fantasy_content.league[0];
+  return (await resp.json()).fantasy_content.league;
 }
 
 export async function getLeagueStatsForWeek(week: string = "current") {
   const resp = await fetch(
-    `https://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.${constants.LEAGUE_ID}/teams/stats;type=week;week=${week}`, {
+    `https://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.${constants.LEAGUE_ID}/teams/stats;type=week;week=${week}?format=json_f`, {
       headers: {
         'Authorization': await getAuth()
       }
     }
   );
 
-  return (await parseStringPromise(await resp.text())).fantasy_content.league[0];
+  return (await resp.json()).fantasy_content.league;
 }
