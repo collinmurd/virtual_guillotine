404b2aac51f8681200e27cdb7ecfb72419bcc17a
Collin Murdock <collinmurdock22@gmail.com>
2024-10-19 14:36:35 +0000

    support for codespances

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   experimental: {
     typedRoutes: true,
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'https://${process.env.CODESPACE_NAME}-3000.github.dev'
+      ]
+    }
   },
   reactStrictMode: false,
   output: 'standalone',
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -10,7 +10,10 @@ export async function logIn() {
   // redirect to request_auth
   if (!session) {
     const client_id = process.env.CLIENT_ID!;
-    const redirect_uri = `${headers().get('Origin')}/guillotine/handle-login`;
+    let redirect_uri = `${headers().get('Origin')}/guillotine/handle-login`;
+    if (process.env.CODESPACE_NAME) {
+      redirect_uri = `${process.env.CODESPACE_NAME}/guillotine/handle-login`;
+    }
     const response_type = 'code';
     redirect(`https://api.login.yahoo.com/oauth2/request_auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}`)
   }
