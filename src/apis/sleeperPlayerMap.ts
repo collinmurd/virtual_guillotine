94bc8088e86a1fc317fba8a5ff23486003309e46
collinmurd <collinmurdock22@gmail.com>
2024-10-25 22:44:00 -0400

    fix player map

--- src/apis/sleeperPlayerMap.ts
+++ src/apis/sleeperPlayerMap.ts
@@ -2,6 +2,11 @@
 import * as fs from 'fs';
 import { SleeperPlayer } from "./sleeper";
 
-const playerMap: { [player_id: string]: SleeperPlayer } = JSON.parse(fs.readFileSync('data/sleeperPlayerMap.json', 'utf8'));
+const PATH = 'data/sleeperPlayerMap.json'
+
+let playerMap: { [player_id: string]: SleeperPlayer } = {}
+if (fs.existsSync(PATH)) {
+  playerMap = JSON.parse(fs.readFileSync(PATH, 'utf8'));
+}
 
 export default playerMap;
